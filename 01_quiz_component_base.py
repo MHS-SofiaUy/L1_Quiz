5dd1635e39a92e5166f26d5132bebc99e5dae4cf
MHS-SofiaUy <86634615+MHS-SofiaUy@users.noreply.github.com>
2022-07-28 12:36:22 +1200

    Add files via upload

--- 01_quiz_component_base.py
+++ 01_quiz_component_base.py
@@ -1,7 +1,54 @@
 import random
 
+# colour codes (thanks daniel for sending this and ms gottschalk for making it)
+
+
+class Colour_It():
+
+    def __init__(self,red,green,blue,text):
+
+        self.red = red
+
+        self.green = green
+
+        self.blue = blue
+
+        self.text = text
+
+    def get_color_escape(self,red,green,blue):
+
+        return '\033[{};2;{};{};{}m'.format(38,red,green,blue)
+
+    def print_colour(self):
+
+        all_coloured = self.get_color_escape(self.red,self.green,self.blue) + self.text
+
+        return all_coloured
+
+    def reset_colour(): # Resets the colours
+
+        return "\033[0m"
+
+# ******** Colours ********
+# watermelon colour aesthetic
+# Turquoise Text
+
+
+def colour_turquoise(message):
+
+    return Colour_It(61, 209, 182, message).print_colour()
+
+# pink text
+
+
+def colour_pink(message):
+
+    return Colour_It(235, 75, 122, message).print_colour()
+
 # Functions go here...
 from tkinter import Y
+
+
 def yes_no(question):
     valid = False
     while not valid:
@@ -18,34 +65,39 @@ def yes_no(question):
         
         else:
             print ("Please answer yes / no, if you're not sure I recommend you to press no")
-  
+
+
 def instructions ():
-    print ("Welcome to uys0083's Trivia Game of Basic Math Equations!~")
     print ()
-    print ("- In this game, the user will have a choice to answer a math equation of the following:")
+    print (colour_turquoise(" ***** ~Welcome to uys0083's Trivia Game of Basic Math Equations!~ ***** {}".format(Colour_It.reset_colour())))
     print ()
-    print ("- Addition, Subtraction, Multiplication and Division!")
+    print (colour_pink("- In this game, the user will have a choice to answer a math equation of the following:{}".format(Colour_It.reset_colour())))
     print ()
-    print ("- The user will select the amount of rounds they wish to play, or infinite mode if they wish to do so.")
+    print (colour_pink("- Addition, Subtraction, Multiplication and Division!{}".format(Colour_It.reset_colour())))
     print ()
-    print ("- For each question, the user gets to choose which category the user wants to play with.")
+    print (colour_pink("- The user will select the amount of rounds they wish to play, or infinite mode if they wish to do so.{}".format(Colour_It.reset_colour())))
     print ()
-    print ("- After the user chooses the category of their choice, the computer will generate a random question from that category.")
+    print (colour_pink("- For each question, the user gets to choose which category the user wants to play with.{}".format(Colour_It.reset_colour())))
     print ()
-    print ("- For every question, the user gets 1 attempt on answering the question correctly.")
+    print (colour_pink("- After the user chooses the category of their choice, the computer will generate a random question from that category.{}".format(Colour_It.reset_colour())))
     print ()
-    print ("- When all the rounds are finished, or the user chooses to quit")
-    print ("  The game will tally up the results and display the user's game statistics.")
+    print (colour_pink("- For every question, the user gets 1 attempt on answering the question correctly.{}".format(Colour_It.reset_colour())))
     print ()
-    print ("- We wish the user to have fun!~")
+    print (colour_pink("- When all the rounds are finished, or the user chooses to quit{}".format(Colour_It.reset_colour())))
+    print (colour_pink("  The game will tally up the results and display the user's game statistics.{}".format(Colour_It.reset_colour())))
+    print ()
+    print (colour_pink("- We wish the user to have fun!~{}".format(Colour_It.reset_colour())))
+    print ()
     return ""
 
 # Main Routine goes here...
 
 # Functions go here
+
+
 def check_rounds():
     while True:
-        response = input("How many rounds: ")
+        response = input(colour_pink("How many rounds would you like to play?: ".format(Colour_It.reset_colour())))
 
         round_error = "Please type either <enter> or an integer that is more than 0"
         
@@ -63,8 +115,13 @@ def check_rounds():
             except ValueError:
                 print(round_error)
                 continue
-            
+        
+        # If infinite mode chosen
+        elif response != "<enter>":
+            print("You have selected infinite mode. Type 'xxx' if at any time you wish to quit.")
+
         return response
+
 
 def choice_checker(question, valid_list, error):
 
@@ -83,6 +140,7 @@ def choice_checker(question, valid_list, error):
         # output error if item not in list
         print(error)
         print()
+
 
 def int_check(question, low=None, high=None):
 
@@ -120,34 +178,31 @@ def int_check(question, low=None, high=None):
 
 # Ask user if they have played before
 # If 'no', show instructions
-played_before = yes_no("Have you played this game before?")
+played_before = yes_no (colour_turquoise("Have you played this game before? {}".format(Colour_It.reset_colour())))
 
 
 if played_before == "no":
     instructions()
 
-print("program continues")
-
 game_summary = []
 
 rounds_lost = 0
 rounds_won = 0
-rounds_played = 0
+rounds_played = rounds_won + rounds_lost
 
 # Lists of valid responses
 yes_no_list = ["yes", "no"]
 equation_list = ["addition", "subtraction", "multiplication", "division", "xxx"]
 
-GUESSES_ALLOWED = 1
-response = ""
-
-already_guessed = []
-guesses_left = GUESSES_ALLOWED
-
 end_game = "no"
 
 # ask user for # of rounds, <enter> for infinite mode
 rounds = check_rounds()
+
+# Choose your equation category
+choose_instruction = "Please choose addition, subtraction, multiplication, division or 'xxx' to exit "
+print()
+choose_error = ("{} or 'xxx' to end: ".format(choose_instruction))
 
 # Testing to generate the numbers for the equation
 while end_game == "no":
@@ -156,14 +211,6 @@ while end_game == "no":
     table_num = random.randint(1, 12)
     table_num2 = random.randint(1, 12)
     ans = table_num * table_num2
-
-    already_guessed = []
-    rounds_played += 1
-
-    choose_instruction = "Please choose addition, subtraction, multiplication, division or 'xxx' to exit "
-    print()
-    choose_error = ("{} or 'xxx' to end: ".format(choose_instruction))
-
 
     # ask user for choice and check it's valid
     choose = choice_checker(choose_instruction, equation_list, choose_error)
@@ -174,7 +221,6 @@ while end_game == "no":
     if choose == "addition":
         answer = int_check("Question: {} + {}  = ".format(chosen_num, chosen_num2))
         actual_ans = chosen_num + chosen_num2
-
 
     # If the user chooses subtraction
     elif choose == "subtraction":
@@ -194,12 +240,16 @@ while end_game == "no":
     # compare user's input answer to the actual answer
     if answer == actual_ans:
         print("That's the correct answer. Well played.")
+        print()
         rounds_won += 1
+        rounds_played += 1
     else:
         print("User Response: {}".format(answer))
         print("Correct Answer: {}".format(actual_ans))
         print("No that's wrong!")
+        print()
         rounds_lost += 1
+        rounds_played += 1
 
     # End game if requested # of rounds has been played
     if rounds_played == rounds:
@@ -213,20 +263,20 @@ percent_win = rounds_won / rounds_played * 100
 percent_lose = rounds_lost / rounds_played * 100
 
 print()
-print("***** Math Quiz History *****")
+print(colour_turquoise("***** Math Quiz History *****{}".format(Colour_It.reset_colour())))
 for game in game_summary:
     print(game)
 
 print()
 
 # displays game stats with % values to the nearest whole number
-print("***** Quiz Statistics *****")
-print("Win: {}, ({:.0f}%) \nLoss: {}, ({:.0f}%)".format(rounds_won, percent_win, rounds_lost, percent_lose))
+print(colour_turquoise("***** Quiz Statistics ***** {}".format(Colour_It.reset_colour())))
+print(colour_pink("Win: {}, ({:.0f}%) \nLoss: {}, ({:.0f}%)".format(rounds_won, percent_win, rounds_lost, percent_lose,(Colour_It.reset_colour()))))
 
 # End of game statements
 print()
-print('**** End Quiz Summary ****')
-print("Won: {} \t|\t Lost: {}".format(rounds_won, rounds_lost))
+print(colour_turquoise("**** End Quiz Summary **** {}".format(Colour_It.reset_colour())))
+print(colour_pink("Won: {} \t|\t Lost: {}".format(rounds_won, rounds_lost,(Colour_It.reset_colour()))))
 print()
-print ("Thanks for playing!")
+print(colour_pink("Thanks for playing! {}".format(Colour_It.reset_colour())))
 
